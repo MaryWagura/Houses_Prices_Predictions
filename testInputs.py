dab3a48b0ee513d21a18a5efaf75a330fc1930a3
clauds <waguramarie@gmail.com>
2025-10-23 17:44:42 +0200

    test temp

--- testInputs.py
+++ testInputs.py
@@ -1,4 +1,10 @@
-test = input('Input a number')
-user_input = int(test)
-float_input = user_input/2
-print('Your value is', float_input)
+# test = input('Input a number')
+# user_input = int(test)
+# float_input = user_input/2
+# print('Your value is', float_input)
+
+
+test = float(input('input a temperature in C:'))
+if(test >= -278.15):
+    c = test - 278
+    print('The temperature is', c)
