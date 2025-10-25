c096c056996445e2f7b8e0ce25a192924e9c2f54
clauds <waguramarie@gmail.com>
2025-10-25 11:50:40 +0200

    boolean

--- testInputs.py
+++ testInputs.py
@@ -4,7 +4,13 @@
 # print('Your value is', float_input)
 
 
-test = float(input('input a temperature in C:'))
-if(test >= -278.15):
-    c = test - 278
-    print('The temperature is', c)
+# test = float(input('input a temperature in C:'))
+# if(test >= -278.15):
+#     c = test - 278
+#     print('The temperature is', c)
+
+input_number = int(input('input a number:'))
+if input_number % 2 == 0:
+    print("That's an even number ")
+else :
+    print ("That's an odd number")
