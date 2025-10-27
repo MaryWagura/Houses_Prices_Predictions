48bf9c6cc1d895fc5670d2015f4a8903ead369a4
clauds <waguramarie@gmail.com>
2025-10-27 08:42:49 +0100

    determine if prime number

--- testInputs.py
+++ testInputs.py
@@ -1,21 +1,23 @@
+#Determine if a float
 # test = input('Input a number')
 # user_input = int(test)
 # float_input = user_input/2
 # print('Your value is', float_input)
 
-
+#Determind the temp
 # test = float(input('input a temperature in C:'))
 # if(test >= -278.15):
 #     c = test - 278
 #     print('The temperature is', c)
 
+#Dtermine if Even/Odd
 # input_number = int(input('input a number:'))
 # if input_number % 2 == 0:
 #     print("That's an even number ")
 # else :
 #     print ("That's an odd number")
 
-
+#Determine if Prime
 n= int (input("Enter a number: "))
 is_prime = True
 for k in range (2,n):
